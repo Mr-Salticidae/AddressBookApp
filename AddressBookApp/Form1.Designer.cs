627af52611f5f89c18cde4e705b1ae1b38b1417e
Chu WANG <wc1225827513@gmail.com>
2021-12-08 17:11:06 +0800

    Build Login UI

--- AddressBookApp/Form1.Designer.cs
+++ AddressBookApp/Form1.Designer.cs
@@ -29,34 +29,93 @@
         private void InitializeComponent()
         {
             this.checkConnection = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // checkConnection
             // 
             this.checkConnection.AutoSize = true;
-            this.checkConnection.Location = new System.Drawing.Point(232, 198);
+            this.checkConnection.Location = new System.Drawing.Point(60, 61);
             this.checkConnection.Name = "checkConnection";
-            this.checkConnection.Size = new System.Drawing.Size(55, 15);
+            this.checkConnection.Size = new System.Drawing.Size(71, 15);
             this.checkConnection.TabIndex = 0;
-            this.checkConnection.Text = "label1";
+            this.checkConnection.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password";
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.Location = new System.Drawing.Point(133, 58);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(100, 25);
+            this.txt_Username.TabIndex = 2;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(133, 97);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(100, 25);
+            this.txt_Password.TabIndex = 3;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(104, 149);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(75, 33);
+            this.btn_Login.TabIndex = 4;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Login);
+            this.groupBox1.Controls.Add(this.checkConnection);
+            this.groupBox1.Controls.Add(this.txt_Password);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_Username);
+            this.groupBox1.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(109, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 221);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkConnection);
+            this.ClientSize = new System.Drawing.Size(508, 450);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label checkConnection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- AddressBookApp/Form1.cs
+++ AddressBookApp/Form1.cs
@@ -13,15 +13,16 @@ namespace AddressBookApp
 {
     public partial class Form1 : Form
     {
+        private OleDbConnection connection = new OleDbConnection();
+        
         public Form1()
         {
             InitializeComponent();
+            connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=./AddressBook.mdb;Persist Security Info=False;";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            OleDbConnection connection = new OleDbConnection();
-            connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=./AddressBook.mdb;Persist Security Info=False;";
             connection.Open();
             checkConnection.Text = "Succeed";
             connection.Close();
